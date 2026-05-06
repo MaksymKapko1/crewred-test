24e37706ff61baf432d69998fb053435092824c9
maksym <kapkomaxim@gmail.com>
2026-05-06 16:21:32 +0300

    Add caching

--- app/services.py
+++ app/services.py
@@ -1,17 +1,22 @@
 import httpx
 from fastapi import HTTPException, status
 
-from app.core.config import settings
+_cache: dict[int, dict] = {}
 
+async def validate_artwork(external_id: int) -> dict:
+    if external_id in _cache:
+        return _cache[external_id]
 
-async def validate_artwork(external_id: int):
-    url = f"{settings.ARTIC_BASE_URL}/artworks/{external_id}"
     async with httpx.AsyncClient() as client:
-        response = await client.get(url)
-
+        response = await client.get(
+            f"https://api.artic.edu/api/v1/artworks/{external_id}",
+            params={"fields": "id,title"}
+        )
         if response.status_code != 200:
             raise HTTPException(
                 status_code=status.HTTP_400_BAD_REQUEST,
                 detail=f"Artwork ID {external_id} not found in Chicago Art API"
             )
-        return response.json()["data"]
+        data = response.json()["data"]
+        _cache[external_id] = data
+        return data
